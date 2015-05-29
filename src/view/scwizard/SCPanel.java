e5815e4ce6e69b8d6d501bbf5b97940decee7455
fdsmora <fausto.ds.mora@gmail.com>
2015-05-28 22:56:59 -0500

    removed unnecessary files and folders

--- src/view/scwizard/SCPanel.java
+++ src/view/scwizard/SCPanel.java
@@ -1,5 +1,50 @@
 package view.scwizard;
 
-public class SCPanel {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import configuration.Constants;
+import de.jreality.plugin.basic.ViewShrinkPanelPlugin;
+import de.jtem.jrworkspace.plugin.Controller;
+import de.jtem.jrworkspace.plugin.PluginInfo;
+
+/***
+ * 
+ * @author Fausto
+ *
+ */
+public class SCPanel extends ViewShrinkPanelPlugin implements ActionListener {
+
+	Step startStep = null;
+	Step currentStep = null;
+	Map<String, Step> steps;
+	
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void initializeWizard(){
+		steps = new HashMap<String, Step>();
+		steps.put(Constants.NUMBER_OF_PROCESSES_STEP, Step.createStep(Constants.NUMBER_OF_PROCESSES_STEP, this));
+		// .. los demás
+	}
+	
+	
+	public PluginInfo getPluginInfo() {
+		return new PluginInfo("Simplicial Complex Panel");
+	}
+	
+	public void install(Controller c) throws Exception{
+		super.install(c);
+	}
+	
+	public void uninstall(Controller c) throws Exception {
+		super.uninstall(c);
+	}
+	
 }
--- src/view/scwizard/Step.java
+++ src/view/scwizard/Step.java
@@ -1,5 +1,12 @@
 package view.scwizard;
 
-public class Step {
+import configuration.Constants;
 
+public abstract class Step {
+		
+	public static Step createStep(String kind, SCPanel p){
+		if (kind.equals(Constants.NUMBER_OF_PROCESSES_STEP)){
+			return new NumberOfProcessesStep(p);
+		}
+	}
 }
