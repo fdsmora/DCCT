cbd1c081ac31ff401609ac70a24ff6f0dc0fc99a
fdsmora <fausto.ds.mora@gmail.com>
2015-05-29 13:28:02 -0500

    Removing unnecessary files

--- src/dcct/topology/CommunicationModel.java
+++
@@ -1,8 +0,0 @@
-package dcct.topology;
-
-import java.util.Set;
-
-public interface CommunicationModel {
-	Set<Simplex> communicationRound(Set<Simplex> simplices);
-	
-}
--- src/test_polygon/PointSequence.java
+++
@@ -1,9 +0,0 @@
-package test_polygon;
-
-import javax.swing.event.ChangeListener;
-
-public interface PointSequence {
-	double[][] getPoints();
-	boolean isClosed();
-	void addChangeListener(ChangeListener cl);
-}
